e62cab324e21023020b2aa969711469eb5c9a08d
Byron Rau <byronrau@brau02-lt-2.voceracommunications.com>
2020-10-01 08:44:58 -0700

    Updates and remove keys

--- twitterUser/twitterUserInfo.py
+++ twitterUser/twitterUserInfo.py
@@ -4,10 +4,8 @@ import sys
 import os
 import codecs
 import unicodecsv as csv
+from ..config import API_KEY, API_SECRET
 
-# API and ACCESS KEYS
-API_KEY = 'jz3feMK2gN0kaN377FsTXY7uY'
-API_SECRET = 'sGfCEayfwORloC9SvHy6BmDjifUsUEIF0EF51SgiYUgs054n7H'
 
 # Don't buffer stdout, so we can tail the log output redirected to a file
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
@@ -18,32 +16,34 @@ fName = sys.argv[1]
 # List of users read from users.txt
 users = []
 
-#open users.txt file and gets the list of users
+# open users.txt file and gets the list of users
 with open('users.txt', 'r') as f:
-  for line in f:
-    users.append(line.strip())
+    for line in f:
+        users.append(line.strip())
 
 auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 if (not api):
-  print ("Can't Authenticate Bye!")
-  sys.exit(-1)
+    print("Can't Authenticate Bye!")
+    sys.exit(-1)
 
 with open(fName, 'ab') as f:
-  writer = csv.writer(f, encoding='utf-8')
-  writer.writerow(['Name','description','location','created_at','id_str','favourites_count','url','profile_image_url_https','listed_count','followers_count','time_zone','statuses_count','friends_count','screen_name'])
+    writer = csv.writer(f, encoding='utf-8')
+    writer.writerow(['Name', 'description', 'location', 'created_at', 'id_str', 'favourites_count', 'url', 'profile_image_url_https',
+                     'listed_count', 'followers_count', 'time_zone', 'statuses_count', 'friends_count', 'screen_name'])
 
-  try:      
-    user_profiles = api.lookup_users(screen_names=users)
-    
-    if not user_profiles:
-      print("No users found")
-    
-    for u in user_profiles:
-      writer.writerow([u.name,u.description,u.location,u.created_at,u.id_str,u.favourites_count,u.url,u.profile_image_url_https,u.listed_count,u.followers_count,u.time_zone,u.statuses_count,u.friends_count,u.screen_name])
+    try:
+        user_profiles = api.lookup_users(screen_names=users)
 
-  except tweepy.TweepError as e:
-    # Just exit if any error
-    print("some error : " + str(e))
+        if not user_profiles:
+            print("No users found")
+
+        for u in user_profiles:
+            writer.writerow([u.name, u.description, u.location, u.created_at, u.id_str, u.favourites_count, u.url, u.profile_image_url_https,
+                             u.listed_count, u.followers_count, u.time_zone, u.statuses_count, u.friends_count, u.screen_name])
+
+    except tweepy.TweepError as e:
+        # Just exit if any error
+        print("some error : " + str(e))
